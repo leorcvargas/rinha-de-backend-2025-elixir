a8fe34f81aabdcad09b27836433807c105626aa1
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2025-08-13 14:28:30 -0300

    api: local buffer counter

--- lib/rinhex/local_buffer.ex
+++ lib/rinhex/local_buffer.ex
@@ -1,9 +1,10 @@
 defmodule Rinhex.LocalBuffer do
   use GenServer
+  require Logger
+
   alias Rinhex.WorkerController
 
   @table :payment_buffer
-
   @flush_interval 50
   @dummy_key 0
 
@@ -11,7 +12,7 @@ defmodule Rinhex.LocalBuffer do
     GenServer.start_link(__MODULE__, state, name: __MODULE__)
   end
 
-  def init(_state) do
+  def init(_) do
     :ets.new(@table, [
       :named_table,
       :public,
@@ -20,13 +21,22 @@ defmodule Rinhex.LocalBuffer do
     ])
 
     Process.send_after(self(), :flush, @flush_interval)
-    {:ok, %{}}
+    {:ok, %{counter: 0}}
   end
 
   @compile {:inline, enqueue: 1}
   def enqueue(raw_body) do
     :ets.insert(@table, {@dummy_key, raw_body})
+    GenServer.cast(__MODULE__, :increment_counter)
     :ok
+  end
+
+  def handle_cast(:increment_counter, state) do
+    counter = state.counter + 1
+
+    Logger.info("Total items seen: #{counter}")
+
+    {:noreply, %{state | counter: counter}}
   end
 
   def handle_info(:flush, state) do
@@ -41,7 +51,14 @@ defmodule Rinhex.LocalBuffer do
     end
 
     Process.send_after(self(), :flush, @flush_interval)
-
     {:noreply, state}
+  end
+
+  def handle_call(:get_counter, _from, state) do
+    {:reply, state.counter, state}
+  end
+
+  def get_counter do
+    GenServer.call(__MODULE__, :get_counter)
   end
 end
