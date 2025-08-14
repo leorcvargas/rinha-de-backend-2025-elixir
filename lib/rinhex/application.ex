1e86d5f2c6dda67efd56cc863f57c2b22cc64d3e
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2025-08-14 17:48:10 -0300

    http_server: pure impl

--- lib/rinhex/application.ex
+++ lib/rinhex/application.ex
@@ -41,22 +41,23 @@ defmodule Rinhex.Application do
               ]
             },
             Rinhex.LocalBuffer,
-            {
-              Bandit,
-              http_1_options: [
-                clear_process_dict: false,
-                gc_every_n_keepalive_requests: 20_000
-              ],
-              http_options: [compress: false],
-              plug: RinhexWeb.HttpServer,
-              scheme: :http,
-              ip: {:local, socket_path},
-              port: 0
-              # thousand_island_options: [
-              #   num_acceptors: 1,
-              #   num_connections: 1024 * 8
-              # ]
-            },
+            Rinhex.HttpServer,
+            # {
+            #   Bandit,
+            #   http_1_options: [
+            #     clear_process_dict: false,
+            #     gc_every_n_keepalive_requests: 20_000
+            #   ],
+            #   http_options: [compress: false],
+            #   plug: RinhexWeb.HttpServerOld,
+            #   scheme: :http,
+            #   ip: {:local, socket_path},
+            #   port: 0
+            #   # thousand_island_options: [
+            #   #   num_acceptors: 1,
+            #   #   num_connections: 1024 * 8
+            #   # ]
+            # },
             {Task, fn -> wait_and_chmod!(socket_path, 0o777) end}
           ]
 
--- lib/rinhex/http_server.ex
+++ lib/rinhex/http_server.ex
@@ -1,36 +1,114 @@
-defmodule RinhexWeb.HttpServer do
-  @behaviour Plug
-
-  import Plug.Conn
-
+defmodule Rinhex.HttpServer do
   alias Rinhex.{LocalBuffer, WorkerController}
 
-  @req_len 512
+  @http_204 "HTTP/1.1 204 No Content\r\n\r\n"
+  @http_200 "HTTP/1.1 200 OK\r\n"
+  @http_200_json "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n"
+  @http_404 "HTTP/1.1 404 Not Found\r\n\r\n"
+  @pong_response @http_200 <> "Content-Length: 4\r\n\r\npong"
 
-  @impl true
-  def init(opts), do: opts
-
-  @impl true
-  def call(%Plug.Conn{method: "POST", path_info: ["payments"]} = conn, _opts) do
-    conn = send_resp(conn, 204, "")
-
-    {:ok, iodata_body, conn} =
-      read_body(
-        conn,
-        length: @req_len,
-        read_length: @req_len
-      )
-
-    LocalBuffer.enqueue(iodata_body)
-
-    conn
+  def child_spec(opts) do
+    %{
+      id: __MODULE__,
+      start: {__MODULE__, :start_link, [opts]},
+      type: :worker,
+      restart: :permanent,
+      shutdown: 5000
+    }
   end
 
-  def call(%Plug.Conn{method: "GET", path_info: ["payments-summary"]} = conn, _opts) do
-    conn = fetch_query_params(conn)
+  def start_link(opts \\ []) do
+    socket_path =
+      System.get_env("UDS_SOCKET") ||
+        Keyword.get(opts, :socket_path) ||
+        "/tmp/rinha.sock"
 
-    from = conn.params["from"]
-    to = conn.params["to"]
+    pid =
+      spawn_link(fn ->
+        Process.flag(:trap_exit, true)
+        start_server(socket_path)
+
+        receive do
+          {:EXIT, _from, reason} ->
+            File.rm(socket_path)
+            exit(reason)
+        end
+      end)
+
+    {:ok, pid}
+  end
+
+  defp start_server(socket_path) do
+    File.rm(socket_path)
+    File.mkdir_p!(Path.dirname(socket_path))
+
+    opts = [
+      :binary,
+      {:ifaddr, {:local, socket_path}},
+      {:packet, :raw},
+      {:active, false},
+      {:backlog, 1024},
+      {:exit_on_close, false}
+    ]
+
+    case :gen_tcp.listen(0, opts) do
+      {:ok, listen_socket} ->
+        File.chmod!(socket_path, 0o666)
+
+        # 2 acceptors for 0.35 vCPU
+        spawn_link(fn -> acceptor_loop(listen_socket) end)
+        spawn_link(fn -> acceptor_loop(listen_socket) end)
+
+        listen_socket
+
+      {:error, reason} ->
+        exit({:listen_failed, reason})
+    end
+  end
+
+  defp acceptor_loop(listen_socket) do
+    case :gen_tcp.accept(listen_socket) do
+      {:ok, socket} ->
+        handle_request(socket)
+        acceptor_loop(listen_socket)
+
+      {:error, :closed} ->
+        :ok
+
+      {:error, _} ->
+        :timer.sleep(10)
+        acceptor_loop(listen_socket)
+    end
+  end
+
+  defp handle_request(socket) do
+    # Read with same timeout as @req_len approach
+    case :gen_tcp.recv(socket, 0, 500) do
+      {:ok, data} ->
+        response = route_request(data)
+        :gen_tcp.send(socket, response)
+
+      _ ->
+        :ok
+    end
+
+    :gen_tcp.close(socket)
+  end
+
+  defp route_request(<<"POST /payments HTTP/1.", _version::size(8), _rest::binary>> = request) do
+    case extract_body(request) do
+      {:ok, body} ->
+        LocalBuffer.enqueue(body)
+
+      _ ->
+        :ok
+    end
+
+    @http_204
+  end
+
+  defp route_request(<<"GET /payments-summary", rest::binary>>) do
+    {from, to} = parse_query_params(rest)
 
     summary_json =
       :erpc.call(
@@ -41,13 +119,62 @@ defmodule RinhexWeb.HttpServer do
         5_000
       )
 
-    conn
-    |> put_resp_content_type("application/json")
-    |> send_resp(200, summary_json)
+    content_length = IO.iodata_length(summary_json)
+
+    [
+      @http_200_json,
+      "Content-Length: ",
+      Integer.to_string(content_length),
+      "\r\n\r\n",
+      summary_json
+    ]
   end
 
-  def call(%Plug.Conn{method: "GET", path_info: ["ping"]} = conn, _opts),
-    do: send_resp(conn, 200, "pong")
+  defp route_request(<<"GET /ping HTTP/1.", _::binary>>) do
+    @pong_response
+  end
 
-  def call(conn, _opts), do: send_resp(conn, 404, "")
+  defp route_request(_) do
+    @http_404
+  end
+
+  defp extract_body(request) do
+    case :binary.split(request, <<"\r\n\r\n">>) do
+      [_headers, body] when byte_size(body) > 0 ->
+        {:ok, body}
+
+      _ ->
+        {:error, :no_body}
+    end
+  end
+
+  defp parse_query_params(rest) do
+    case rest do
+      <<"?", query_rest::binary>> ->
+        # Extract query string before HTTP version
+        query =
+          case :binary.split(query_rest, " HTTP/") do
+            [q, _] -> q
+            _ -> ""
+          end
+
+        # Parse from and to params
+        params = parse_query_string(query)
+        {params["from"], params["to"]}
+
+      _ ->
+        {nil, nil}
+    end
+  end
+
+  defp parse_query_string(query_string) do
+    query_string
+    |> String.split("&")
+    |> Enum.reduce(%{}, fn pair, acc ->
+      case String.split(pair, "=", parts: 2) do
+        [key, value] -> Map.put(acc, key, URI.decode(value))
+        _ -> acc
+      end
+    end)
+  end
 end
