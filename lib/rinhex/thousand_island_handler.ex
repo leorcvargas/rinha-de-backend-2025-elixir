6e2745520e9b3d069107f1c282cfecb938818d59
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2025-08-14 18:48:33 -0300

    keep alive

--- lib/rinhex/thousand_island_handler.ex
+++ lib/rinhex/thousand_island_handler.ex
@@ -2,11 +2,14 @@ defmodule Rinhex.ThousandIslandHandler do
   use ThousandIsland.Handler
   alias Rinhex.{LocalBuffer, WorkerController}
 
-  @http_204 "HTTP/1.1 204 No Content\r\n\r\n"
-  @http_200_json "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\n"
-  @http_200 "HTTP/1.1 200 OK\r\nContent-Length: 4\r\n\r\npong"
-  @http_404 "HTTP/1.1 404 Not Found\r\n\r\n"
-  @http_503 "HTTP/1.1 503 Service Unavailable\r\n\r\n"
+  @http_204_keepalive "HTTP/1.1 204 No Content\r\nConnection: keep-alive\r\n\r\n"
+  @http_204_close "HTTP/1.1 204 No Content\r\nConnection: close\r\n\r\n"
+  @http_200_json_keepalive "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nConnection: keep-alive\r\n"
+  @http_200_json_close "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nConnection: close\r\n"
+  @http_200_keepalive "HTTP/1.1 200 OK\r\nConnection: keep-alive\r\nContent-Length: 4\r\n\r\npong"
+  @http_200_close "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Length: 4\r\n\r\npong"
+  @http_404 "HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n"
+  @http_503 "HTTP/1.1 503 Service Unavailable\r\nConnection: close\r\n\r\n"
 
   @recv_timeout 30_000
   @max_requests_per_connection 1000
@@ -23,11 +26,12 @@ defmodule Rinhex.ThousandIslandHandler do
   defp handle_requests(socket, state, count) do
     case ThousandIsland.Socket.recv(socket, 0, @recv_timeout) do
       {:ok, data} ->
-        response = route_request(data)
+        keep_alive = should_keep_alive?(data) and count < @max_requests_per_connection - 1
+        response = route_request(data, keep_alive)
 
         case ThousandIsland.Socket.send(socket, response) do
           :ok ->
-            if should_keep_alive?(data) do
+            if keep_alive do
               handle_requests(socket, state, count + 1)
             else
               {:close, state}
@@ -58,9 +62,9 @@ defmodule Rinhex.ThousandIslandHandler do
     end
   end
 
-  defp route_request(data) do
+  defp route_request(data, keep_alive) do
     try do
-      do_route_request(data)
+      do_route_request(data, keep_alive)
     rescue
       error ->
         IO.inspect(error, label: "Route error")
@@ -69,7 +73,7 @@ defmodule Rinhex.ThousandIslandHandler do
     end
   end
 
-  defp do_route_request(<<"POST /payments HTTP/1.", _::binary>> = request) do
+  defp do_route_request(<<"POST /payments HTTP/1.", _::binary>> = request, keep_alive) do
     case extract_body(request) do
       {:ok, body} ->
         LocalBuffer.enqueue(body)
@@ -78,10 +82,10 @@ defmodule Rinhex.ThousandIslandHandler do
         :ok
     end
 
-    @http_204
+    if keep_alive, do: @http_204_keepalive, else: @http_204_close
   end
 
-  defp do_route_request(<<"GET /payments-summary", rest::binary>>) do
+  defp do_route_request(<<"GET /payments-summary", rest::binary>>, keep_alive) do
     {from, to} = parse_query_params(rest)
 
     case :erpc.call(:rinhex@worker, WorkerController, :get_payments_summary, [from, to], 20_000) do
@@ -92,24 +96,25 @@ defmodule Rinhex.ThousandIslandHandler do
             "fallback" => %{"totalRequests" => 0, "totalAmount" => 0.0}
           })
 
-        build_json_response(empty_summary)
+        build_json_response(empty_summary, keep_alive)
 
       summary_json when is_binary(summary_json) or is_list(summary_json) ->
-        build_json_response(summary_json)
+        build_json_response(summary_json, keep_alive)
     end
   end
 
-  defp do_route_request(<<"GET /ping HTTP/1.", _::binary>>) do
-    @http_200
+  defp do_route_request(<<"GET /ping HTTP/1.", _::binary>>, keep_alive) do
+    if keep_alive, do: @http_200_keepalive, else: @http_200_close
   end
 
-  defp do_route_request(_) do
+  defp do_route_request(_, _keep_alive) do
     @http_404
   end
 
-  defp build_json_response(json_data) do
+  defp build_json_response(json_data, keep_alive) do
     content_length = IO.iodata_length(json_data)
-    [@http_200_json, "Content-Length: ", Integer.to_string(content_length), "\r\n\r\n", json_data]
+    header = if keep_alive, do: @http_200_json_keepalive, else: @http_200_json_close
+    [header, "Content-Length: ", Integer.to_string(content_length), "\r\n\r\n", json_data]
   end
 
   defp extract_body(request) do
