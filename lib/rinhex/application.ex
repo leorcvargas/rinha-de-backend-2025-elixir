0a1cd9eae0a820d4d7b39483993d7a195efd96e3
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2025-08-14 02:45:15 -0300

    gc every 500 keep alive

--- lib/rinhex/application.ex
+++ lib/rinhex/application.ex
@@ -45,7 +45,7 @@ defmodule Rinhex.Application do
               Bandit,
               http_1_options: [
                 # clear_process_dict: false
-                gc_every_n_keepalive_requests: 50
+                gc_every_n_keepalive_requests: 500
                 # gc_every_n_keepalive_requests: 20_000
               ],
               http_options: [compress: false],
