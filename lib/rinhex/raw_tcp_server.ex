aaabdb0233947223337f4692d955d90c444ffc47
Leonardo Vargas <leocarvalho.vargas@gmail.com>
2025-08-15 01:28:07 -0300

    Revert "take keep alive in consideration"
    This reverts commit f7324a882f4edf70e5d7bb327abdc6b3cbc2a1ef.

--- lib/rinhex/raw_tcp_server.ex
+++ lib/rinhex/raw_tcp_server.ex
@@ -2,11 +2,8 @@ defmodule Rinhex.RawTcpServer do
   require Logger
   alias Rinhex.{LocalBuffer, WorkerController}
 
-  @http_204_keepalive "HTTP/1.1 204 No Content\r\nConnection: keep-alive\r\n\r\n"
-  @http_204_close "HTTP/1.1 204 No Content\r\nConnection: close\r\n\r\n"
-  @http_200_json_keepalive "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nConnection: keep-alive\r\n"
-  @http_200_json_close "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nConnection: close\r\n"
-  @max_requests_per_connection 10_000
+  @http_204 "HTTP/1.1 204 No Content\r\nConnection: keep-alive\r\n\r\n"
+  @http_200_json "HTTP/1.1 200 OK\r\nContent-Type: application/json\r\nConnection: keep-alive\r\n"
 
   def child_spec(opts) do
     %{
@@ -26,10 +23,8 @@ defmodule Rinhex.RawTcpServer do
   end
 
   defp init_persistent_responses do
-    :persistent_term.put(:http_204_keepalive, @http_204_keepalive)
-    :persistent_term.put(:http_204_close, @http_204_close)
-    :persistent_term.put(:http_200_json_keepalive, @http_200_json_keepalive)
-    :persistent_term.put(:http_200_json_close, @http_200_json_close)
+    :persistent_term.put(:http_204, @http_204)
+    :persistent_term.put(:http_200_json, @http_200_json)
   end
 
   defp accept_loop(_opts) do
@@ -74,28 +69,16 @@ defmodule Rinhex.RawTcpServer do
   end
 
   defp handle_connection(socket) do
-    handle_connection_loop(socket, 0)
-  end
-
-  defp handle_connection_loop(socket, request_count) do
     case :gen_tcp.recv(socket, 0, 5000) do
       {:ok, data} ->
-        should_close = process_request(socket, data, request_count + 1)
-
-        if should_close or request_count + 1 >= @max_requests_per_connection do
-          :gen_tcp.close(socket)
-        else
-          handle_connection_loop(socket, request_count + 1)
-        end
+        process_request(socket, data)
 
       _ ->
         :gen_tcp.close(socket)
     end
   end
 
-  defp process_request(socket, <<"POST /payments", _::binary>> = data, _request_count) do
-    should_close = should_close_connection?(data)
-
+  defp process_request(socket, <<"POST /payments", _::binary>> = data) do
     case :binary.match(data, <<"\r\n\r\n">>) do
       {pos, 4} ->
         body_start = pos + 4
@@ -109,19 +92,11 @@ defmodule Rinhex.RawTcpServer do
         :ok
     end
 
-    response =
-      if should_close do
-        :persistent_term.get(:http_204_close)
-      else
-        :persistent_term.get(:http_204_keepalive)
-      end
-
-    :gen_tcp.send(socket, response)
-    should_close
+    :gen_tcp.send(socket, :persistent_term.get(:http_204))
+    :gen_tcp.close(socket)
   end
 
-  defp process_request(socket, <<"GET /payments-summary", rest::binary>>, _request_count) do
-    should_close = should_close_connection?(<<"GET /payments-summary", rest::binary>>)
+  defp process_request(socket, <<"GET /payments-summary", rest::binary>>) do
     {from, to} = parse_query_params(rest)
 
     # Call worker for summary
@@ -130,29 +105,21 @@ defmodule Rinhex.RawTcpServer do
         empty_summary =
           "{\"default\":{\"totalRequests\":0,\"totalAmount\":0.0},\"fallback\":{\"totalRequests\":0,\"totalAmount\":0.0}}"
 
-        send_json_response(socket, empty_summary, should_close)
+        send_json_response(socket, empty_summary)
 
       summary_json when is_binary(summary_json) or is_list(summary_json) ->
-        send_json_response(socket, summary_json, should_close)
+        send_json_response(socket, summary_json)
     end
-
-    should_close
   end
 
-  defp process_request(socket, _data, __request_count) do
+  defp process_request(socket, _) do
     :gen_tcp.send(socket, "HTTP/1.1 404 Not Found\r\nConnection: close\r\n\r\n")
-    true
+    :gen_tcp.close(socket)
   end
 
-  defp send_json_response(socket, json_data, should_close) do
+  defp send_json_response(socket, json_data) do
     content_length = IO.iodata_length(json_data)
-
-    header =
-      if should_close do
-        :persistent_term.get(:http_200_json_close)
-      else
-        :persistent_term.get(:http_200_json_keepalive)
-      end
+    header = :persistent_term.get(:http_200_json)
 
     response = [
       header,
@@ -163,18 +130,21 @@ defmodule Rinhex.RawTcpServer do
     ]
 
     :gen_tcp.send(socket, response)
+    :gen_tcp.close(socket)
   end
 
   defp parse_query_params(rest) do
     try do
       case rest do
         <<"?", query_rest::binary>> ->
+          # Find end of query string
           query =
             case :binary.match(query_rest, <<" HTTP/">>) do
               {pos, _} -> :binary.part(query_rest, 0, pos)
               :nomatch -> ""
             end
 
+          # Simple parsing for from= and to=
           from = extract_param(query, "from=")
           to = extract_param(query, "to=")
           {from, to}
@@ -204,13 +174,6 @@ defmodule Rinhex.RawTcpServer do
 
       :nomatch ->
         nil
-    end
-  end
-
-  defp should_close_connection?(data) do
-    case :binary.match(data, <<"Connection: close">>) do
-      {_, _} -> true
-      :nomatch -> false
     end
   end
 end
